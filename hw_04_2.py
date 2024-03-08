56bd9bcac2550ee62ac8699c70019493c3609786
Julia Zahorovska <ju.zagorovsky@gmail.com>
2024-03-08 16:41:15 +0100

    clean task 2 code

--- hw_04_2.py
+++ hw_04_2.py
@@ -4,18 +4,21 @@ dir_path = PurePath('__file__').parent
 
 def get_cats_info(path):
 
-    cats = list()
+    cats = []
 
     try:
-        with open(f'{path}/data/cats.txt', 'r', encoding='utf-8') as fh:
+        with open(path.joinpath('data', 'cats.txt'), 'r', encoding='utf-8') as fh:
             for line in fh.readlines():
                 cat_data = line.strip().split(',')
                 cats.append({'id': cat_data[0], 'name': cat_data[1], 'age': cat_data[2]})
     
     except FileNotFoundError:
         print('Вказаного файлу cats.txt не знайдено')
+        return []
     
-    print(f'Наші коти:\n{cats[0]}\n{cats[1]}\n{cats[2]}\n{cats[3]}\n{cats[4]}')
+    for ind, cat in enumerate(cats):
+        print(f'Кіт {ind+1}: {cat}')
+
     return cats
 
 if __name__ == '__main__':
