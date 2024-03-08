caf24f07f47e788ed3b241adb4edeff58f5aae1a
Julia Zahorovska <ju.zagorovsky@gmail.com>
2024-03-08 18:09:23 +0100

    cleans code

--- hw_04_1.py
+++ hw_04_1.py
@@ -1,28 +1,45 @@
-from pathlib import PurePath
-
-dir_path = PurePath(__file__).parent
+from pathlib import Path
 
 def total_salary(path):
+
+    if not path.exists():
+        print('Вказаний файл не знайдено')
+        return None
+    
+    if not path.is_file():
+        print('Вказаний шлях веде не до файлу')
+        return None
     
     try:
-        with open(f'{path}/data/salaries.txt', 'r', encoding='utf-8') as fh:
+        with open(path, 'r', encoding='utf-8') as fh:
             total = 0
             workers = 0
             
-            for item in fh.readlines():
-                _, value = item.strip().split(',')
-                total += int(value)
-                workers += 1
+            for ind, line in enumerate(fh, start=1):
+                try:
+                    _, value = line.strip().split(',')
+                    if value.isdigit():
+                        total += int(value)
+                        workers += 1
+                    else:
+                        raise ValueError
+                
+                except ValueError:
+                    print(f'Неправильне значення зарплати у рядку {ind}')
+                    return None
+            
+            if workers == 0:
+                print('Файл порожній')
+                return None
             
             average = total // workers
             print(f"Для {workers} робітників загальна сума заробітної плати: {total}, середня заробітна плата: {average}")
             return [total, average]
     
     except FileNotFoundError:
-        print('Вказаного файлу salaries.txt не знайдено')
-
-    except ValueError:
-        print('Значення заробітної плати не є числом')
+        print('Вказаного файлу не знайдено')
+        return None
 
 if __name__ == '__main__':
+    dir_path = Path('data/salaries.txt')
     total_salary(dir_path)
--- hw_04_2.py
+++ hw_04_2.py
@@ -1,25 +1,22 @@
-from pathlib import PurePath
-
-dir_path = PurePath('__file__').parent
+from pathlib import Path
 
 def get_cats_info(path):
 
     cats = []
 
     try:
-        with open(path.joinpath('data', 'cats.txt'), 'r', encoding='utf-8') as fh:
-            for line in fh.readlines():
+        with open(path, 'r', encoding='utf-8') as fh:
+            for ind, line in enumerate(fh):
                 cat_data = line.strip().split(',')
-                cats.append({'id': cat_data[0], 'name': cat_data[1], 'age': cat_data[2]})
+                cat = {'id': cat_data[0], 'name': cat_data[1], 'age': cat_data[2]}
+                cats.append(cat)
+                print(f'Кіт {ind}: {cat}')
+        return cats
     
     except FileNotFoundError:
         print('Вказаного файлу cats.txt не знайдено')
         return []
-    
-    for ind, cat in enumerate(cats):
-        print(f'Кіт {ind+1}: {cat}')
-
-    return cats
 
 if __name__ == '__main__':
+    dir_path = Path('data/cats.txt')
     get_cats_info(dir_path)
